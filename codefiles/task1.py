bde1a4faf9c5c3a486aa673fb62ab3f4e1b20dd0
VTiganov <vadimyxy@gmail.com>
2024-12-08 13:46:06 +0300

    input matrix

--- codefiles/task1.py
+++ codefiles/task1.py
@@ -1,11 +1,25 @@
-
+from typing import List, Optional
 
 class matrixKeeper():
     
-    def trace():
+    def __init__(self):
+        self.matrix: Optional[List[List[float]]] = None
+
+    def inputMatrix(self) -> None:
+        """Ввод матрицы
+        Первое приглашение - ввод размера матрицы n X m
+        Второе приглашение - ввод самой матрицы по строчке, элементы через проблем"""
+        
+        n, m = map(int, input("Введите размер матрицы n x m: ").split())
+        self.matrix = []
+        for _ in range(n):
+            row = list(map(float, input().split()))
+            self.matrix.append(row)
+
+    def trace(self) -> float:
         """Поиск следа матрицы"""
         pass
 
-    def findByIndex() -> int:
+    def findByIndex(self, n: int, m: int) -> float:
         """Находит элемент в матрице по индексу вида n,m и выводит его"""
         pass
